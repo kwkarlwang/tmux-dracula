2b7b9eb67c26e2da721aad324541d6ba397a4dcc
Karl Wang <kawang@ucsd.edu>
2021-07-21 23:27:57 -0700

    update

--- scripts/dracula.sh
+++ scripts/dracula.sh
@@ -246,7 +246,7 @@ main()
 
   fi
 
-  tmux set-window-option -g window-status-format "#[fg=${white}]#[bg=${gray}] #I #W${flags}"
+  tmux set-window-option -g window-status-format "#[fg=${white}]#[bg=${dark_gray}] #I #W${flags}"
   tmux set-window-option -g window-status-activity-style "bold"
   tmux set-window-option -g window-status-bell-style "bold"
 }
